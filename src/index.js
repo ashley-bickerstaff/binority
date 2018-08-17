6410b91ca616e2fa0252922d052f9bde07e17a24
Ashley Bickerstaff <ashley.bickerstaff@workplacesystems.com>
2018-08-17 09:07:10 +0100

    Adding material provider

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { RMWCProvider } from 'rmwc/Provider';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <RMWCProvider>
+        <App />
+    </RMWCProvider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
