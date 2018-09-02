5023fc18fe1fb49f026b26a43762cef55cbce36b
Ashley Bickerstaff <ashleybickerstaff@hotmail.com>
2018-09-02 17:48:35 +0100

    Added delete functionality

--- src/TodoList.js
+++ src/TodoList.js
@@ -11,6 +11,7 @@ class TodoList extends Component {
         };
 
         this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
     }
 
     addItem(e) {
@@ -27,9 +28,19 @@ class TodoList extends Component {
             });
 
             this.taskNameInput.value = "";
-            console.log(this.state.items);
             e.preventDefault();
         }
+    }
+
+    deleteItem(key) {
+        var filteredItems = this.state.items.filter(function (item) {
+            return item.key !== key;
+        });
+
+        this.setState({
+                items: filteredItems
+            }
+        );
     }
 
     render() {
@@ -46,7 +57,10 @@ class TodoList extends Component {
                         <button type="submit">add</button>
                     </form>
                 </div>
-                <TodoItems entries={this.state.items} />
+                <TodoItems
+                    entries={this.state.items}
+                    delete={this.deleteItem}
+                />
             </div>
         );
     }
